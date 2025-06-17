8c6962e02658a7e92f917e81bc49635316c48165
Daniel Jakubiec <danielj@post.com>
2025-06-16 21:25:19 -0400

    aliases

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+alias eg='env | grep'
+alias ls='ls -G'
+alias ycmconf='touch .ycm_extra_conf.py'
+alias pid='ps -e | grep' 
+alias grep='grep --color'
+alias vf='vim `fzf`'
+alias cdr='cd $(git rev-parse --show-toplevel)'
+
